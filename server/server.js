7c3581c806c5fc38685824c8bad1bbaffa490ee8
AnishMane <anishmane70@gmail.com>
2024-05-28 09:10:22 +0530

    c2

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.URI)
+.then(
+    () => {
+        console.log("connected");
+        app.listen(process.env.PORT || 8000, (err)=>{
+            if(err) console.log(err);
+
+            console.log("running successfully at", process.env.PORT)
+        });
+    }
+)
+.catch((error) => {
+    console.log("error", error);
+});
+
 app.get("/", (req,res)=> {
     res.send("api running")
-})
-
-app.listen(4000);
+});
